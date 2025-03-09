87f9bdc0939748d6d163d21a9fa1792b403a99f5
lakshaybxt <bishtlakshay14@gmail.com>
2025-03-09 22:38:14 +0530

    Make a todo list

--- script/11-todo-list.js
+++ script/11-todo-list.js
@@ -1,33 +1,47 @@
 const todoList = [];
 
+const input = document.querySelector('.js-input');
+const dateInput = document.querySelector('.js-date');
+
 renderTodoList();
 
 function renderTodoList() {
     let todoListHTML = ``;
 
-    for(let i=0; i<todoList.length; i++) {
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
-        todoListHTML += html;
+    for(let i = 0; i<todoList.length; i++) {
+        const todoObject = todoList[i];
+        // const name = todoObject.name;
+        // const dueDate = todoObject.dueDate;
+        const {name, dueDate} = todoObject;
+        todoListHTML += `
+            <div>${name}</div>
+            <div>${dueDate}</div>
+            <button class="delete-todo-button" onclick="
+                todoList.splice(${i}, 1);
+                renderTodoList();
+            ">Delete</button>
+        `;
     }
 
-    console.log(todoListHTML);
+    document.querySelector('.js-text').innerHTML = todoListHTML;
 
-    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
-
-const button = document.querySelector('.js-name-input');
-button.addEventListener('keydown', (event) => {
+input.addEventListener('keydown', (event) => {
     if(event.key === 'Enter') addTodo();
-});
+}); 
 
 function addTodo() {
-    const inputElement = document.querySelector('.js-name-input');
-    const name = inputElement.value.trim();
-    todoList.push(name);
-    console.log(todoList);
+    const name = input.value;
+    const dueDate = dateInput.value;
+    todoList.push({
+        // name: name, 
+        // duedate: duedate
+        name,
+        dueDate
+    });
 
-    inputElement.value = '';
+    console.log(todoList);
+    input.value = ``;
 
     renderTodoList()
 }
